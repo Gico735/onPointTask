88c6dad41658547c292d84b102f06e8bc28a9844
Gregory <gic0@bk.ru>
2018-01-11 20:50:01 +0300

    панигиляция

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-//$(document).ready(function(){
+$(document).ready(function(){
+
+//прокрутка экрана
 //инициализация свайпов
 $(".wrapper").swipe( {
         swipeUp:upSwipe,
@@ -10,14 +12,34 @@ $(".wrapper").swipe( {
 function upSwipe(event){
     let $window = $(window);
     let top = $window.scrollTop()+768;
+    if (top == 768){
+        $('.pagination-circle-active:eq(0)').fadeOut();
+        $('.pagination-circle-active:eq(1)').fadeIn();
+    }
+    else {
+        $('.pagination-circle-active:eq(1)').fadeOut();
+        $('.pagination-circle-active:eq(2)').fadeIn();
+    }
         $('body,html').animate({scrollTop: top}, 500);
-    
+        
 }
 function downSwipe(event){
     let $window = $(window);
     let top = $window.scrollTop()-768;
+    if (top == 768){
+        $('.pagination-circle-active:eq(2)').fadeOut();
+        $('.pagination-circle-active:eq(1)').fadeIn();
+    }
+    else {
+        $('.pagination-circle-active:eq(1)').fadeOut();
+        $('.pagination-circle-active:eq(0)').fadeIn();
+    }
         $('body,html').animate({scrollTop: top}, 500);
 }
 
 
-//});
+//пагинация
+
+
+    
+});
